2926c1be5559b0d13b8ade6fb78553187235c56d
Murilo Alves Pereira Varoto <66957888+Muriloapv@users.noreply.github.com>
2026-04-08 15:31:14 -0300

    Update app.py

--- scraping_teste/app.py
+++ scraping_teste/app.py
@@ -1,12 +1,11 @@
 import requests
-from bs4 import BeatifulSoup
+from bs4 import BeautifulSoup
 
 pagina = requests.get('https://quotes.toscrape.com/')
-dados_pagina = BeatifulSoup( pagina.text, 'html.parser' )
-
+dados_pagina = BeautifulSoup(pagina.text, 'html.parser')
 
 todas_frases = dados_pagina.find_all('div', class_="quote")
 
 for div in todas_frases:
-   text = div.find( 'span', class_='text' )
-   print(div)
+    text = div.find('span', class_='text')
+    print(text.text)
